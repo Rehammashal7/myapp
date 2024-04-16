122f60228148811dbf26cf447b86406c21912916
Rehammashal7 <rehammashal7@gmail.com>
2024-04-16 20:08:49 +0200

    delete useless pages

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
 import Welcome from './pages/Welcome';
 import LoginScreen from './pages/LoginScreen'
 import SignUpScreen from './pages/SignUpScreen'
@@ -8,19 +7,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import adminprofile from './adminPages/adminprofile';
 import favorite from './pages/favorite';
-// import{ profile} from './pages/profile';
 import profile from './pages/profile';
-import { BurgerDetails, ProductsListBurger } from './pages/Burgers';
-import { CoffeeDetails, ProductsListCoffee } from './pages/Drink';
 import { adminBurgerDetails, adminProductsListBurger, EditProductPage } from './adminPages/adminBurger'
 import { CoffeeDetailsAdmin, ProductsListCoffeeAdmin, EditCoffeePage } from './adminPages/adminDrink';
 import { PizzaDetailsAdmin, ProductsListPizzaAdmin, EditPizzaPage } from './adminPages/adminPizza';
 import Checkout from './pages/Checkout';
 import EditProfile from './pages/EditProfile';
-import { PizzaDetails, ProductsListPizza } from './pages/ProductsListPizza';
 import AddProductForm from './adminPages/AddProductForm';
 import products from './pages/AddProducts'
-import { OfferDetails, ProductsListOffer } from './pages/Offers';
+
 import { OfferDetailsAdmin, ProductsListOfferAdmin, EditOfferPage } from './adminPages/OfferAdmin';
 import CartScreen from './pages/CartScreen'
 import adminHome from './adminPages/adminHome'
@@ -31,8 +26,6 @@ import AddReviewWoman from './pages/AddReviews/AddReview-woman';
 import AddReviewMen from './pages/AddReviews/AddReview-men';
 import AddReviewKids from './pages/AddReviews/AddReview-kids';
 import AddReviewBaby from './pages/AddReviews/AddReview-baby';
-
-
 import category from './pages/catigory';
 import DeleteAccount from './pages/DeleteAccount';
 // import MapScreen from './pages/MapScreen';
@@ -43,7 +36,6 @@ import {WomanDetails, ProductsListWoman } from './pages/Catigory/Woman';
 import { MenDetails,ProductsListMen } from './pages/Catigory/Men';
 import { KidsDetails,ProductsListKids } from './pages/Catigory/Kids';
 import { BabyDetails,ProductsListBaby } from './pages/Catigory/Baby';
-
 import PayWithCard from './pages/PayWithCard';
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -53,33 +45,23 @@ export default function App() {
       <Stack.Screen name="PayWithCard" component={PayWithCard} />
         <Stack.Screen name="Welcome" component={Welcome} />
         <Stack.Screen name="Home" component={HomeScreen} />
-
         <Stack.Screen name="WOMAN" component={ProductsListWoman} />
         <Stack.Screen name="WomanDetails" component={WomanDetails} />
         <Stack.Screen name='AddReviewWoman' component={AddReviewWoman}/>
-
         <Stack.Screen name="MEN" component={ProductsListMen} />
         <Stack.Screen name="MenDetails" component={MenDetails} />
         <Stack.Screen name='AddReviewMen' component={AddReviewMen}/>
-
         <Stack.Screen name="KIDS" component={ProductsListKids} />
         <Stack.Screen name="KidsDetails" component={KidsDetails} />
         <Stack.Screen name='AddReviewKids' component={AddReviewKids}/>
-
         <Stack.Screen name="BABY" component={ProductsListBaby} />
         <Stack.Screen name="BabyDetails" component={BabyDetails} />
         <Stack.Screen name='AddReviewBaby' component={AddReviewBaby}/>
-
         <Stack.Screen name="AllReviewsPage" component={AllReviewsPage} />
-
         <Stack.Screen name="adminBurgers" component={adminProductsListBurger} />
         <Stack.Screen name='AddProducts' component={AddProducts}/>
         <Stack.Screen name='OrderHistory' component={OrderHistory}/>
         <Stack.Screen name="AddProductForm" component={AddProductForm} />
-        <Stack.Screen name="PizzaDetails" component={PizzaDetails}  />
-        <Stack.Screen name="OfferDetails" component={OfferDetails}  />
-        <Stack.Screen name="CoffeeDetails" component={CoffeeDetails}  />
-        <Stack.Screen name="BurgerDetails" component={BurgerDetails}  />
         <Stack.Screen name="adminBurgerDetails" component={adminBurgerDetails}  />
         <Stack.Screen name="favorite" component={favorite} />
         <Stack.Screen name="Login" component={LoginScreen} />
@@ -101,17 +83,12 @@ export default function App() {
         <Stack.Screen name="catigory" component={ category }/>
         <Stack.Screen name="EditProfile" component={ EditProfile }/>
         <Stack.Screen name="DeleteAccount" component={ DeleteAccount }/>
-        {/* <Stack.Screen name="MapScreen" component={ MapScreen }/> */}
-
-
         <Stack.Screen name="adminPizza" component={ ProductsListPizzaAdmin }/>
         <Stack.Screen name="EditPizzaPage" component={ EditPizzaPage }/>
-
         <Stack.Screen name="addadmin" component={ addadmin }/>
         <Stack.Screen name="plusbutton" component={ plusbutton }/>
         <Stack.Screen name="checkout" component={ Checkout }/>
         <Stack.Screen name=" BottomNavigator" component={  BottomNavigator }/>
-        
 
       </Stack.Navigator>
     </NavigationContainer>
