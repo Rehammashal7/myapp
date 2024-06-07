e9a8789240ef7d927bea3d92f0dbea5bf70e6802
walaa244 <walaaesmail91@gmail.com>
2024-06-07 05:51:00 +0300

    neww

--- pages/Chatbot.js
+++ pages/Chatbot.js
@@ -175,8 +175,8 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Button } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
-import Dialogflow from 'react-native-dialogflow';
-import Voice from 'react-native-voice';
+import {Dialogflow_V2} from 'react-native-dialogflow';
+ import Voice from 'react-native-voice';
 import { dialogflowConfig } from '../env';
 import axios from 'axios';
 
@@ -184,7 +184,7 @@ import axios from 'axios';
 const Chatbot = () => {
   const [messages, setMessages] = useState([]);
   const [isListening, setIsListening] = useState(false);
-  const botAvatar = require('../assets/Avatar.PNG');
+  const botAvatar = require('../assets/Avatar.png');
   const BOT = {
     _id: 2,
     name: 'Mr.Bot',
@@ -192,10 +192,10 @@ const Chatbot = () => {
   };
 
   useEffect(() => {
-    Dialogflow.setConfiguration(
+    Dialogflow_V2.setConfiguration(
       dialogflowConfig.client_email,
       dialogflowConfig.private_key,
-      Dialogflow.LANG_ENGLISH_US,
+      Dialogflow_V2.LANG_ENGLISH_US,
       dialogflowConfig.project_id
     );
 
@@ -222,165 +222,21 @@ const Chatbot = () => {
       Voice.destroy().then(Voice.removeAllListeners);
     };
   }, []);
-  // const handleSend = (newMessage = []) => {
-  //   setMessages((previousMessages) =>
-  //     GiftedChat.append(previousMessages, newMessage)
-  //   );
-  //   const text = newMessage[0].text;
-  
-  //   // إعداد بيانات الطلب لـ Dialogflow
-  //   const requestData = {
-  //     query: text,
-  //     lang: 'en', // لغة الطلب
-  //     sessionId: '12345' // يمكنك استخدام معرّف الجلسة الخاص بك
-  //   };
-  
-  //   axios.post('https://api.api.ai/v1/query?v=20150910', requestData, {
-  //     headers: {
-  //       'Authorization': `Bearer ${dialogflowConfig.accessToken}`, 
-  //       'Content-Type': 'application/json',
-  //       'origin': 'http://localhost:8080', 
-  //       'x-requested-with': 'XMLHttpRequest' 
-  //     }
-  //   })
-  //   .then(response => {
-  //     handleResponse(response.data);
-  //   })
-  //   .catch(error => {
-  //     console.log(error);
-  //   });
-  // };
-  // const handleSend = (newMessage = []) => {
-  //   setMessages((previousMessages) =>
-  //     GiftedChat.append(previousMessages, newMessage)
-  //   );
-  //   const text = newMessage[0].text;
-  //   Dialogflow.requestQuery(
-  //     text,
-  //     result => handleResponse(result),
-  //     error => console.log(error),
-  //     {
-  //       origin: 'http://localhost:8080', // تحديد رأس origin
-  //       'x-requested-with': 'XMLHttpRequest' // تحديد رأس x-requested-with
-  //     }
-  //   );
-  // };
- 
+
+
   const handleSend = (newMessage = []) => {
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, newMessage)
     );
     const text = newMessage[0].text;
-    fetch('https://api.api.ai/v1/query?v=20150910', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Origin': 'http://localhost:19006',
-        'X-Requested-With': 'XMLHttpRequest',
-        'charset': 'utf-8'
-      },
-      body: JSON.stringify({
-        query: text,
-        lang: 'en',
-        sessionId: 'somerandomsessionid'
-      })
-    })
-    .then(response => response.json())
-    .then(result => handleResponse(result))
-    .catch(error => console.log(error));
+
+  
+Dialogflow_V2.requestQuery(
+      text,
+      result => handleResponse(result),
+      error => console.log(error)
+    );
   };
-  
-
-//   const handleSend = (newMessage = []) => {
-//     setMessages((previousMessages) =>
-//       GiftedChat.append(previousMessages, newMessage)
-//     );
-//     const text = newMessage[0].text;
-// //     fetch('https://your-firebase-project-id.cloudfunctions.net/dialogflowProxy', {
-// //   method: "POST",
-// //   headers: {
-// //     "Content-Type": "application/json"
-// //   },
-// //   body: JSON.stringify({
-// //     query: text,
-// //     lang: "en",
-// //     sessionId: dialogflowConfig.project_id
-// //   })
-// // })
-// // .then(response => response.json())
-// // .then(result => handleResponse(result))
-// // .catch(error => console.log(error));
-// Dialogflow.requestQuery(
-//     text,
-//     result => handleResponse(result),
-//     error => console.log(error),
-//     {
-//       origin: 'http://localhost:8080', // تحديد رأس origin
-//       'x-requested-with': 'XMLHttpRequest' // تحديد رأس x-requested-with
-//     }
-//   );
-  
-// // Dialogflow.requestQuery(
-// //     `http://localhost:8080/https://api.api.ai/v1/query?v=20150910&query=${encodeURIComponent(text)}`,
-// //     result => handleResponse(result),
-// //     error => console.log(error)
-// //   );
-  
-//     // Dialogflow.requestQuery(
-//     //   text,
-//     //   result => handleResponse(result),
-//     //   error => console.log(error)
-//     // );
-//   };
-
-// const handleSend = async (newMessage = []) => {
-//     try {
-//       const apiUrl = 'https://api.dialogflow.com/v1/query';
-      
-//       // استخدم Axios لإرسال الطلب POST إلى API
-//       const response = await axios.post(apiUrl, {
-//         query: newMessage[0].text,
-//         lang: 'en',
-//         sessionId: dialogflowConfig.project_id
-//       }, {
-//         headers: {
-//           Authorization: `Bearer ${dialogflowConfig.client_email}`,
-//           'Content-Type': 'application/json; charset=utf-8'
-//         }
-//       });
-  
-//       // استخدم الدالة handleResponse لمعالجة الرد
-//       handleResponse(response.data);
-//     } catch (error) {
-//       console.error('Error:', error);
-//     }
-//   };
-
-
-// const handleSend = async (newMessage = []) => {
-//     try {
-//      // const apiUrl = 'https://dialogflow.googleapis.com/v2/projects/' + dialogflowConfig.project_id + '/agent/sessions/123456789:detectIntent';
-//       const apiUrl = 'https://api.api.ai/v1/query?v=20150910';
-
-      
-//           const response = await axios.post(apiUrl, {
-//         query: newMessage[0].text,
-//         lang: dialogflowConfig.private_key,
-//         sessionId: "9a8c1699-746e-2173-4973-4a3807034b1c"
-//       }, {
-//         headers: {
-//           Authorization: `Bearer ${dialogflowConfig.client_email}`,
-//           'Content-Type': 'application/json; charset=utf-8'
-//         }
-//       }
-    
-//     );
-
-//       handleResponse(response.data);
-//     } catch (error) {
-//       console.error('Error:', error);
-//     }
-//   };
 
 
   const handleResponse = (result) => {
