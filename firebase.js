5d87777ce76cf19d3390fde654b55f309c160f92
Rehammashal7 <rehammashal7@gmail.com>
2024-06-20 02:53:17 +0300

    .

--- firebase.js
+++ firebase.js
@@ -24,20 +24,28 @@ import { collection, getDocs } from 'firebase/firestore';
 //   appId: "1:243055050436:web:126adf43a29d6950305391",
 //   measurementId: "G-MG32J1V30N"
 // };
+// const firebaseConfig = {
+
+//   apiKey: "AIzaSyB6wf8xG6S8diCkCZlu5k2O7Hpjaxk4WH8",
+
+//   authDomain: "test-a74c1.firebaseapp.com",
+
+//   projectId: "test-a74c1",
+
+//   storageBucket: "test-a74c1.appspot.com",
+
+//   messagingSenderId: "438157288823",
+
+//   appId: "1:438157288823:web:5e1bab90b8548f29006d72"
+
+// };
 const firebaseConfig = {
-
-  apiKey: "AIzaSyB6wf8xG6S8diCkCZlu5k2O7Hpjaxk4WH8",
-
-  authDomain: "test-a74c1.firebaseapp.com",
-
-  projectId: "test-a74c1",
-
-  storageBucket: "test-a74c1.appspot.com",
-
-  messagingSenderId: "438157288823",
-
-  appId: "1:438157288823:web:5e1bab90b8548f29006d72"
-
+  apiKey: "AIzaSyCbW6TFYhnqZkdP8JpM4DOgE1EpWuaIgQc",
+  authDomain: "software-engineering-dcd78.firebaseapp.com",
+  projectId: "software-engineering-dcd78",
+  storageBucket: "software-engineering-dcd78.appspot.com",
+  messagingSenderId: "880271325840",
+  appId: "1:880271325840:web:f4c4934be1f8b7e45a3615"
 };
 
 
